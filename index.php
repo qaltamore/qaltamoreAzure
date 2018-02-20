36ae2731c829af68b263a943a77e7ea6f6953fe7
Altamore Quentin <quentin.altamore@estiam.com>
2018-02-20 13:09:59 +0100

    test

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
 <?php 
 	echo "JE HAIS OUTLOOK !!!";
+
+$tmpTab = explode(";", $_SERVER['MYSQLCONNSTR_localdb']);
+$parametersList = array();
+for($i = 0; $i < sizeof($tmpTab); $i++) {
+    $parametersList = explode("=", $tmpTab[$i][1]);
+}
+
+var_dump(parametersList);
 ?>
