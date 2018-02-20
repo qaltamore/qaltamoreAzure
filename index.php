e2097778db1fcff9a1d4f790244dbdc44c52f4a5
Altamore Quentin <quentin.altamore@estiam.com>
2018-02-20 13:18:04 +0100

    en fait non

--- index.php
+++ index.php
@@ -5,6 +5,7 @@ $tmpTab = explode(";", $_SERVER['MYSQLCONNSTR_localdb']);
 $parametersList = array();
 var_dump(sizeof($tmpTab));
 for($i = 0; $i < sizeof($tmpTab); $i++) {
+echo explode("=", $tmpTab[$i])[1] . "<br />";
     array_push($parametersList, explode("=", $tmpTab[$i])[1]);
 }
 
